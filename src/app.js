6142ce16239ef7fd56f75cf99cf063a102b762de
Rafael Santos <rafaelnsantos1@gmail.com>
2018-10-05 17:54:46 -0300

    add cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const expressGraphQL = require('express-graphql')
 const GraphQLSchema = require('./schema')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use(require('./middleware/context'))
 app.use(require('./middleware/player'))
 app.use(require('./middleware/admin'))
