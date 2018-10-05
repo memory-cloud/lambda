cef5a01d33c4f8acd2d9cea787c411d2574967ae
Rafael Santos <rafaelnsantos1@gmail.com>
2018-10-05 18:12:07 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -4,13 +4,12 @@ const GraphQLSchema = require('./schema')
 const cors = require('cors')
 const app = express()
 
-app.use(cors())
 app.use(require('./middleware/context'))
 app.use(require('./middleware/player'))
 app.use(require('./middleware/admin'))
 
 app.use(
-  '/',
+  '/', cors(),
   expressGraphQL((req) => {
     return {
       graphiql: process.env.NODE_ENV === 'development',
