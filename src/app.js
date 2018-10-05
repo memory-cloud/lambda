83fa1180010179cbd17fba0fac5ee4ce7610175f
Rafael Santos <rafaelnsantos1@gmail.com>
2018-10-05 19:20:11 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -1,21 +1,22 @@
 const express = require('express')
 const expressGraphQL = require('express-graphql')
 const GraphQLSchema = require('./schema')
-const cors = require('cors')
 const app = express()
-
+const debug = require('debug')('app')
+const cors = require('cors')
 app.use(require('./middleware/context'))
 app.use(require('./middleware/player'))
 app.use(require('./middleware/admin'))
-
 app.use(
-  '/', cors(),
+  '/',
+  cors(),
   expressGraphQL((req) => {
     return {
       graphiql: process.env.NODE_ENV === 'development',
       context: req.context,
       schema: GraphQLSchema,
       formatError (err) {
+        debug(err)
         return {
           path: err.path ? err.path[0] : undefined,
           message: err.message
